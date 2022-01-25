3268dd40e9869b28b6c19c8c7b5c7766881fbda0
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2022-01-26 01:36:20 +0330

    feat: added tail for mouse

--- src/app/modify/putMouse.ts
+++ src/app/modify/putMouse.ts
@@ -1,7 +1,18 @@
 import mouse from "../utils/mouse";
-import { getPixelCoordByPosition, setPixel } from "../pixels";
+import { getPixelCoordByPosition, setPixel, getPixelColor } from "../pixels";
+
+const previous: { x: number; y: number }[] = [];
 
 export default function putMouse() {
   const pixel = getPixelCoordByPosition(mouse.x, mouse.y);
+
+  previous.push(pixel);
+  if (previous.length > 10) previous.shift();
+
+  previous.forEach((p, index) => {
+    const current = getPixelColor(p.x, p.y);
+    setPixel(p.x, p.y, current + index * 10);
+  });
+
   setPixel(pixel.x, pixel.y, 255);
 }
