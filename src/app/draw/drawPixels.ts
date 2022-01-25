2082036cfcfb1823ec0241a915f92c334c7caed0
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2022-01-26 00:49:22 +0330

    feat: centered pixels

--- src/app/draw/drawPixels.ts
+++ src/app/draw/drawPixels.ts
@@ -5,16 +5,21 @@ import {
   getPixelSize,
   PIXELS_GAP,
 } from "../pixels";
+import screen from "../utils/screen";
 
 const drawPixels: TDraw = (ctx) => {
   for (let y = 0; y < PIXELS_HEIGHT; y++) {
     for (let x = 0; x < PIXELS_WIDTH; x++) {
       const size = getPixelSize();
+      const sizeWithGap = size + PIXELS_GAP;
+
+      const offsetX = screen.width / 2 - (PIXELS_WIDTH * sizeWithGap) / 2;
+      const offsetY = screen.height / 2 - (PIXELS_HEIGHT * sizeWithGap) / 2;
 
       ctx.fillStyle = `white`;
       ctx.fillRect(
-        (size + PIXELS_GAP) * x,
-        (size + PIXELS_GAP) * y,
+        sizeWithGap * x + offsetX,
+        sizeWithGap * y + offsetY,
         size,
         size
       );
