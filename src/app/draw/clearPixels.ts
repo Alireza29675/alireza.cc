ba3310174a88eb15b60c66a65f315fc8a72e0f2b
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-03-26 15:36:38 +0300

    reset all

--- src/app/draw/clearPixels.ts
+++
@@ -1,7 +0,0 @@
-import { TDraw } from "../types";
-
-const clearPixels: TDraw = (ctx) => {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-};
-
-export default clearPixels;
--- src/components/App.tsx
+++
@@ -1,9 +0,0 @@
-import MainCanvas from './MainCanvas'
-
-export default function App() {
-  return (
-    <div className="App">
-      <MainCanvas />
-    </div>
-  )
-}
