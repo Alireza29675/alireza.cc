1569b11d624e3b70c57cabb25ea51bfe5304942d
Alireza <contact@alireza.cc>
2023-09-27 23:42:44 +0200

    feat: switched to miro board !

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+const MIRO_BOARD_ADDRESS = "https://miro.com/app/board/uXjVMzzwMt8=/?share_link_id=875082594130"
+
+export default function App() {
+  return (
+    <div className="App">
+      <iframe
+        style={{
+          position: 'absolute',
+          border: 'none',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%'
+        }}
+        src={MIRO_BOARD_ADDRESS}
+        allow="fullscreen; clipboard-read; clipboard-write"
+      />
+    </div>
+  )
+}
